f94773c6ce663212d319d0107d81d6e0f334012a
nabathnm <muhammadnabathnuur@gmail.com>
2026-09-17 19:31:28 +0700

    fix error merge

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -1,5 +1,3 @@
-import 'package:brawigo/features/marketplace/presentation/pages/marketplace_buyer_page.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/auth/presentation/pages/splash_page.dart';
 import '../../features/auth/presentation/pages/login_page.dart';
@@ -10,9 +8,6 @@ import '../../features/marketplace/presentation/pages/main_screen.dart';
 import '../../features/auth/presentation/pages/set_photo_page.dart';
 import '../../features/auth/presentation/pages/onboarding_success_page.dart';
 import 'package:brawigo/features/marketplace/presentation/pages/marketplace_buyer_page.dart';
-import 'package:brawigo/features/marketplace/presentation/pages/marketplace_seller_page.dart';
-import 'package:brawigo/features/marketplace/presentation/pages/add_product_page.dart';
-import 'package:brawigo/features/profile/presentation/pages/profile_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:brawigo/features/marketplace/presentation/bloc/marketplace_bloc.dart';
 import 'package:brawigo/features/marketplace/presentation/bloc/marketplace_event.dart';
